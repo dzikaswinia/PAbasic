5adb5a7830f90397628b921e48717857a3e285f2
Monika <you@example.com>
2020-09-25 18:03:18 +0200

    my log Datei ist da

--- src/main/java/staciwa/projektarbeit/App_local.java
+++ src/main/java/staciwa/projektarbeit/App_local.java
@@ -1,29 +1,21 @@
 package staciwa.projektarbeit;
 
-import java.util.Map;
 import java.util.function.Function;
 
 import javax.servlet.http.HttpServlet;
 
-import org.eclipse.basyx.aas.manager.ConnectedAssetAdministrationShellManager;
-import org.eclipse.basyx.aas.metamodel.connected.ConnectedAssetAdministrationShell;
 import org.eclipse.basyx.aas.metamodel.map.AssetAdministrationShell;
 import org.eclipse.basyx.aas.metamodel.map.descriptor.AASDescriptor;
 import org.eclipse.basyx.aas.metamodel.map.descriptor.ModelUrn;
 import org.eclipse.basyx.aas.metamodel.map.descriptor.SubmodelDescriptor;
 import org.eclipse.basyx.aas.registration.api.IAASRegistryService;
 import org.eclipse.basyx.aas.registration.memory.InMemoryRegistry;
-import org.eclipse.basyx.aas.registration.proxy.AASRegistryProxy;
 import org.eclipse.basyx.aas.registration.restapi.DirectoryModelProvider;
 import org.eclipse.basyx.aas.restapi.AASModelProvider;
 import org.eclipse.basyx.aas.restapi.VABMultiSubmodelProvider;
 import org.eclipse.basyx.models.controlcomponent.ControlComponent;
 import org.eclipse.basyx.models.controlcomponent.ExecutionState;
-import org.eclipse.basyx.submodel.metamodel.api.ISubModel;
 import org.eclipse.basyx.submodel.metamodel.api.identifier.IdentifierType;
-import org.eclipse.basyx.submodel.metamodel.api.submodelelement.dataelement.IDataElement;
-import org.eclipse.basyx.submodel.metamodel.api.submodelelement.dataelement.IProperty;
-import org.eclipse.basyx.submodel.metamodel.api.submodelelement.operation.IOperation;
 import org.eclipse.basyx.submodel.metamodel.map.SubModel;
 import org.eclipse.basyx.submodel.metamodel.map.submodelelement.dataelement.property.Property;
 import org.eclipse.basyx.submodel.metamodel.map.submodelelement.dataelement.property.valuetypedef.PropertyValueTypeDef;
@@ -34,99 +26,65 @@ import org.eclipse.basyx.vab.modelprovider.VABElementProxy;
 import org.eclipse.basyx.vab.modelprovider.api.IModelProvider;
 import org.eclipse.basyx.vab.modelprovider.lambda.VABLambdaProviderHelper;
 import org.eclipse.basyx.vab.modelprovider.map.VABMapProvider;
-import org.eclipse.basyx.vab.protocol.api.IConnectorProvider;
 import org.eclipse.basyx.vab.protocol.basyx.connector.BaSyxConnector;
 import org.eclipse.basyx.vab.protocol.basyx.server.BaSyxTCPServer;
-import org.eclipse.basyx.vab.protocol.http.connector.HTTPConnectorProvider;
 import org.eclipse.basyx.vab.protocol.http.server.AASHTTPServer;
 import org.eclipse.basyx.vab.protocol.http.server.BaSyxContext;
 import org.eclipse.basyx.vab.protocol.http.server.VABHTTPInterface;
 
-
+/**This class creates and runs an AAS for the Pasteurizator.
+ * 
+ * @author monika
+ * **/
 public class App_local 
 {
+	//GLOBALS: a network address and port of the device on which the AAS is running.
+	public static String AAS_IP = "localhost";		//TODO von User abfragen?
 	public static int CC_PORT = 4001;
 	public static int AAS_PORT = 4000;
-	//public static String AAS_IP = "192.168.2.3";
-	public static String AAS_IP = "192.168.2.3";
 	public static String CC_IP = "";
 	
     public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
-        Pasteurizator pasti = new Pasteurizator("podgrzewacz mleka");
+        
+    	System.out.println("Welcome to Pasteurizator's AAS");
+        Pasteurizator pasti = new Pasteurizator("Milcherhitzter");
         startMyControlComponent(pasti);
         startMyAAS(pasti);
         
-        
-        /*
-        //Connecting to AAS
-        IAASRegistryService registry = new AASRegistryProxy("http://" + App_local.AAS_IP + ":" + App_local.AAS_PORT + "/registry");
-        //IAASRegistryService registry = new AASRegistryProxy("http://192.168.2.3:4000/registry");
-        IConnectorProvider connectorProvider = new HTTPConnectorProvider();
-		ConnectedAssetAdministrationShellManager manager = new ConnectedAssetAdministrationShellManager(registry,
-				connectorProvider);
- 
-		// The ID of the oven AAS
-		ModelUrn aasURN = new ModelUrn("de.FHG", "devices.es.iese", "AAS", "1.0", "1", "oven01", "001");
-		ConnectedAssetAdministrationShell connectedAAS = manager.retrieveAAS(aasURN);
-		Map<String, ISubModel> submodels = connectedAAS.getSubModels();
-		ISubModel connectedTankSM = submodels.get("tank");
-		
-		System.out.print("Short ID: " + connectedTankSM.getIdShort());
-				
-		Map<String, IDataElement> properties = connectedTankSM.getDataElements();
-		IProperty tankProperty = (IProperty) properties.get("maxCapacity");
-		double maxCap = (double) tankProperty.get();
-		System.out.println("\nMax Capacity: " + maxCap);
-		*/
-        
-		// TODO Funktionsaufruf ohne AAS, durch ControlComponent
-		/*
-		VABElementProxy proxy = new VABElementProxy("", new JSONConnector(new BaSyxConnector(App_local.CC_IP, App_local.CC_PORT)));
-		proxy.setModelPropertyValue("status/opMode", PasteurizatorControlComponent.OPMODE_FILL);
-		proxy.invokeOperation("operations/service/start");	
-		*/
-		
-		/*
-		Map<String, IOperation> operations = connectedTankSM.getOperations();
-		IOperation tankOperation = operations.get("fillTank");
-		tankOperation.invoke();
-		
-		
-		
-		System.out.println("Current liguid level in tank: " + pasti.getTank().getCurrentLiquidLevel());
-		System.out.println("Current liguid level in tank: " + pasti.getTank().getCurrentLiquidLevel());
-		System.out.println("Current liguid level in tank: " + pasti.getTank().getCurrentLiquidLevel());
-		//TODO kann nicht die Funktion mehrmal aufrufen
-		tankOperation.invoke();
-		*/
     }
     
+    /**This method starts the Asset Administration Shell.
+     * 
+     * @param pasti: the Pasteurizator
+     */
     public static void startMyAAS(Pasteurizator pasti) {
     	
-    	/**Submodel
+    	/**Submodel: Tank
     	 * 
     	 */
     	SubModel tankSubModel = new SubModel();
-    	// static property
+    	
+    	// static property: maximal capacity of the tank.
     	Property maxCapacity = new Property();
     	maxCapacity.setIdShort("maxCapacity");
     	maxCapacity.set(VABLambdaProviderHelper.createSimple(() -> {
     		return pasti.getTank().getMaxCapacity(); 
     	}, null), PropertyValueTypeDef.Double);
     	
-    	//dynamic property 
+    	//dynamic property: current liquid level in the tank. 
     	Property currentLiquidLevel = new Property();
     	currentLiquidLevel.setIdShort("currentLiquidLevel");
     	currentLiquidLevel.set(VABLambdaProviderHelper.createSimple(() -> {
     		return pasti.getTank().getCurrentLiquidLevel(); 
     	}, null), PropertyValueTypeDef.Double);
     	
+    	tankSubModel.addSubModelElement(maxCapacity);
+    	tankSubModel.addSubModelElement(currentLiquidLevel);
     	
     	// Function 
     	Function<Object[], Object> tankFillInvokable = (params) -> {
-			// From: HandsOn 04
+		
 			// Connect to the control component
 			VABElementProxy proxy = new VABElementProxy("", new JSONConnector(new BaSyxConnector(App_local.CC_IP, App_local.CC_PORT)));
  
@@ -145,31 +103,27 @@ public class App_local
 			}
  
 			proxy.invokeOperation("operations/service/reset");
-			// Then return -> synchronous
 			return null;
 		};
 		
-		// Create the Operation
+		// Creating the Operation
 		Operation operation = new Operation();
 		operation.setIdShort("fillTank");
 		operation.setInvocable(tankFillInvokable);
 		tankSubModel.addSubModelElement(operation);
     	
+		// Setting identifiers. 
     	tankSubModel.setIdShort("tank");
     	tankSubModel.setIdentification(IdentifierType.CUSTOM, "tank");
-    	tankSubModel.addSubModelElement(maxCapacity);
-    	tankSubModel.addSubModelElement(currentLiquidLevel);
-    	
-    	
-    	  	
     	
     	/**
     	 * AAS
     	 */
     	AssetAdministrationShell aas = new AssetAdministrationShell();
-    	ModelUrn aasURN = new ModelUrn("de.FHG", "devices.es.iese", "AAS", "1.0", "1", "oven01", "001");
+    	ModelUrn aasURN = new ModelUrn("de.FHG", "devices.es.iese", "AAS", "1.0", "1", "oven01", "001"); //TODO new name for this device
     	aas.setIdentification(aasURN);
     	
+    	// TODO delete?
     	//IIdentifier aasID = new Identifier(IdentifierType.CUSTOM, "pasti");
     	//aas.setIdentification(aasID);
     	aas.setIdShort("pasti");
@@ -209,9 +163,6 @@ public class App_local
 		
 		httpServer.start();
 		
-		
-		
-		
 		/*
 		//closing the server
 		try {
@@ -224,14 +175,10 @@ public class App_local
 		*/
     }
     
-    /*
-    public static Map<String, Object> createMyPasteurizatorModel(Pasteurizator pasti) {
-    	
-    	Map<String, Object> properties = new HashMap<>();
-    	
-    }
-    */
-    
+    /** This method creates a control component for the Pasteurizator.
+     * 
+     * @param pasti: Pasteurizator
+     */
     public static void startMyControlComponent(Pasteurizator pasti) {
     	
     	ControlComponent cc = new PasteurizatorControlComponent(pasti);
@@ -241,6 +188,5 @@ public class App_local
     	server.start();
     	
     }
-    
     
 }
--- src/main/java/staciwa/projektarbeit/App_remote.java
+++ src/main/java/staciwa/projektarbeit/App_remote.java
@@ -9,46 +9,53 @@ import org.eclipse.basyx.aas.registration.proxy.AASRegistryProxy;
 import org.eclipse.basyx.submodel.metamodel.api.ISubModel;
 import org.eclipse.basyx.submodel.metamodel.api.submodelelement.ISubmodelElement;
 import org.eclipse.basyx.submodel.metamodel.api.submodelelement.dataelement.IProperty;
+import org.eclipse.basyx.submodel.metamodel.api.submodelelement.operation.IOperation;
 import org.eclipse.basyx.vab.protocol.api.IConnectorProvider;
 import org.eclipse.basyx.vab.protocol.http.connector.HTTPConnectorProvider;
 
+/**This class connects to the AAS.
+ * 
+ * @author monika
+ * **/
 public class App_remote 
 {
+	
+	//GLOBALS: a network address and port of the device on which the AAS is running.
+	public static String AAS_IP = "192.168.2.107";
 	public static int CC_PORT = 4001;
 	public static int AAS_PORT = 4000;
-	public static String AAS_IP = "192.168.2.3";
+	
 	
     public static void main( String[] args ) throws Exception
     {
      
         //Connecting to AAS
-        //IAASRegistryService registry = new AASRegistryProxy("http://192.168.2.3:4000/registry/api/v1/registry");
-    	//IAASRegistryService registry = new AASRegistryProxy("http://192.168.2.3:4000/registry");
     	IAASRegistryService registry = new AASRegistryProxy("http://" + App_remote.AAS_IP + ":" 
     			+ App_local.AAS_PORT + "/registry");
         IConnectorProvider connectorProvider = new HTTPConnectorProvider();
 		ConnectedAssetAdministrationShellManager manager = new ConnectedAssetAdministrationShellManager(registry,
 				connectorProvider);
 					
-		// The ID of the oven AAS
-		ModelUrn aasURN = new ModelUrn("de.FHG", "devices.es.iese", "AAS", "1.0", "1", "oven01", "001");
-		//ConnectedAssetAdministrationShell connectedAAS = manager.retrieveAAS(aasURN);
-		//IIdentifier aasID = new Identifier(IdentifierType.CUSTOM, "pasti");
-		//IIdentifier smID = new Identifier(IdentifierType.CUSTOM, "tank");
-		//ISubModel connectedSM = manager.retrieveSubModel(aasID, smID);
+		// The ID of the Pasteurizator AAS
+		ModelUrn aasURN = new ModelUrn("de.FHG", "devices.es.iese", "AAS", "1.0", "1", "oven01", "001");	//TODO change name
 		ConnectedAssetAdministrationShell connectedAAS = manager.retrieveAAS(aasURN);
-		
+		// Retrieving the tank's submodel.
 		Map<String, ISubModel> submodels = connectedAAS.getSubModels();
 		ISubModel connectedTankSM = submodels.get("tank");
-		
 		System.out.print("Short ID: " + connectedTankSM.getIdShort());
-
+		// Retrieving the properties of the submodel.
 		Map<String, ISubmodelElement> properties = connectedTankSM.getSubmodelElements(); 
 		IProperty maxCapPro = (IProperty) properties.get("maxCapacity");
 		Double maxCap = (Double) maxCapPro.get();
 		System.out.print("\nCapacity: " + maxCap);
 		IProperty currentLiquidLevel = (IProperty) properties.get("currentLiquidLevel");
 		Double curLiqLev = (Double) currentLiquidLevel.get();
+		System.out.println("\nCurrent liquid level: " + curLiqLev);
+		// Filling the tank.
+		Map<String, IOperation> operations = connectedTankSM.getOperations();
+		IOperation tankOperation = operations.get("fillTank");
+		tankOperation.invoke();
+		
 		System.out.println("\nCurrent liquid level: " + curLiqLev);
 		
     }
